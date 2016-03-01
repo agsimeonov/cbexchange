e711095f4a31b35533634c7cb9dd1b55a6402525
Alexander Simeonov <agsimeon@buffalo.edu>
2016-02-29 23:13:09 -0500

    Added a module that deals with errors.

--- cbexchange/error.py
+++ cbexchange/error.py
@@ -17,11 +17,11 @@ class APIError(CBExchangeError):
   cause. Your language's http library should be configured to provide message
   bodies for non-2xx requests so that you can read the message field from the
   body. Find more here: https://docs.exchange.coinbase.com/#errors
-  
+
   :param requests.Response response: HTTP error response
-  
+
   """
-  
+
   def __init__(self, response):
     self.status_code = response.status_code
     self.message = response.json().get('message')
@@ -41,11 +41,11 @@ class InternalServerError(APIError): pass  # 500
 
 def get_api_error(response):
   """Acquires the correct error for a given response.
-    
+
   :param requests.Response response: HTTP error response
   :returns: the appropriate error for a given response
   :rtype: APIError
-    
+
   """
   error_class = _status_code_to_class.get(response.status_code, APIError)
   return error_class(response)
