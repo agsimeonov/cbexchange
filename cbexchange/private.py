aeb705e25e612d68f15c7041a26857174a98e1b7
Alexander Simeonov <agsimeon@buffalo.edu>
2016-03-02 16:10:34 -0500

    Finished up Private API.

--- cbexchange/private.py
+++ cbexchange/private.py
@@ -15,10 +15,16 @@ from hashlib import sha256
 from hmac import new
 from time import time
 
-from requests import get
+from requests import delete, get, post
 from requests.auth import AuthBase
 
 from cbexchange.client import RESTClient, PaginationClient
+
+# List Orders Status
+STATUS_ALL     = 'all'
+STATUS_OPEN    = 'open'
+STATUS_PENDING = 'pending'
+
 
 
 class CoinbaseExchangeAuth(AuthBase):
@@ -76,7 +82,12 @@ class PrivateClient(RESTClient):
 
     """
     uri = self._create_api_uri(*relative_path_parts)
-    response = get(uri, auth=self.auth, params=kwargs.get('params', None))
+    if method == 'get':
+      response = get(uri, auth=self.auth, params=kwargs.get('params', None))
+    elif method == 'post':
+      response = post(uri, auth=self.auth, json=kwargs.get('data', None))
+    else:
+      response = delete(uri, auth=self.auth, json=kwargs.get('data', None))
     return self._handle_response(response).json()
 
   def list_accounts(self):
@@ -90,6 +101,169 @@ class PrivateClient(RESTClient):
   def get_account_history(self, account_id):
     """`<https://docs.exchange.coinbase.com/#get-account-history>`_"""
     return self._get('accounts', account_id, 'ledger')
+
+  def get_holds(self, account_id):
+    """`<https://docs.exchange.coinbase.com/#get-holds>`_"""
+    return self._get('accounts', account_id, 'holds')
+
+  def _place_order(self,
+                   side,
+                   product_id='BTC-USD',
+                   client_oid=None,
+                   type=None,
+                   stp=None,
+                   price=None,
+                   size=None,
+                   funds=None,
+                   time_in_force=None,
+                   cancel_after=None,
+                   post_only=None):
+    """`<https://docs.exchange.coinbase.com/#orders>`_"""
+    data = {
+      'side':side,
+      'product_id':product_id,
+      'client_oid':client_oid,
+      'type':type,
+      'stp':stp,
+      'price':price,
+      'size':size,
+      'funds':funds,
+      'time_in_force':time_in_force,
+      'cancel_after':cancel_after,
+      'post_only':post_only
+    }
+    return self._post('orders', data=data)
+
+  def place_limit_order(self,
+                        side,
+                        price,
+                        size,
+                        product_id='BTC-USD',
+                        client_oid=None,
+                        stp=None,
+                        time_in_force=None,
+                        cancel_after=None,
+                        post_only=None):
+    """`<https://docs.exchange.coinbase.com/#orders>`_"""
+    return self._place_order(side,
+                             product_id=product_id,
+                             client_oid=client_oid,
+                             type='limit',
+                             stp=stp,
+                             price=price,
+                             size=size,
+                             time_in_force=time_in_force,
+                             cancel_after=cancel_after,
+                             post_only=post_only)
+
+  def place_market_order(self,
+                         side,
+                         product_id='BTC-USD',
+                         size=None,
+                         funds=None,
+                         client_oid=None,
+                         stp=None):
+    """`<https://docs.exchange.coinbase.com/#orders>`_"""
+    return self._place_order(type='market',
+                             side=size,
+                             product_id=product_id,
+                             size=size,
+                             funds=funds,
+                             client_oid=client_oid,
+                             stp=stp)
+
+  def cancel_order(self, order_id):
+    """`<https://docs.exchange.coinbase.com/#cancel-an-order>`_"""
+    return self._delete('orders', order_id)
+
+  def cancel_all(self, product_id=None):
+    """`<https://docs.exchange.coinbase.com/#cancel-all>`_"""
+    return self._delete('orders', data={'product_id':product_id})
+
+  def list_orders(self, status=None):
+    """`<https://docs.exchange.coinbase.com/#list-orders>`_"""
+    return self._get('orders', params={'status':status})
+
+  def get_order(self, order_id):
+    """`<https://docs.exchange.coinbase.com/#get-an-order>`_"""
+    return self._get('orders', order_id)
+
+  def list_fills(self):
+    """`<https://docs.exchange.coinbase.com/#list-fills>`_"""
+    return self._get('fills')
+
+  def _deposit_withdraw(self, type, amount, coinbase_account_id):
+    """`<https://docs.exchange.coinbase.com/#depositwithdraw>`_"""
+    data = {
+      'type':type,
+      'amount':amount,
+      'coinbase_account_id':coinbase_account_id
+    }
+    return self._post('transfers', data=data)
+
+  def deposit(self, amount, coinbase_account_id):
+    """`<https://docs.exchange.coinbase.com/#depositwithdraw>`_"""
+    return self._deposit_withdraw('deposit', amount, coinbase_account_id)
+
+  def withdraw(self, amount, coinbase_account_id):
+    """`<https://docs.exchange.coinbase.com/#depositwithdraw>`_"""
+    return self._deposit_withdraw('withdraw', amount, coinbase_account_id)
+
+  def _new_report(self,
+                  type,
+                  start_date,
+                  end_date,
+                  product_id='BTC-USD',
+                  account_id=None,
+                  format=None,
+                  email=None):
+    """`<https://docs.exchange.coinbase.com/#create-a-new-report>`_"""
+    data = {
+      'type':type,
+      'start_date':self._format_iso_time(start_date),
+      'end_date':self._format_iso_time(end_date),
+      'product_id':product_id,
+      'account_id':account_id,
+      'format':format,
+      'email':email
+    }
+    return self._post('reports', data=data)
+
+  def new_fills_report(self,
+                       start_date,
+                       end_date,
+                       account_id=None,
+                       product_id='BTC-USD',
+                       format=None,
+                       email=None):
+    """`<https://docs.exchange.coinbase.com/#create-a-new-report>`_"""
+    return self._new_report(start_date,
+                            'fills',
+                            end_date,
+                            account_id,
+                            product_id,
+                            format,
+                            email)
+
+  def new_accounts_report(self,
+                          start_date,
+                          end_date,
+                          account_id,
+                          product_id='BTC-USD',
+                          format=None,
+                          email=None):
+    """`<https://docs.exchange.coinbase.com/#create-a-new-report>`_"""
+    return self._new_report(start_date,
+                            'account',
+                            end_date,
+                            product_id,
+                            account_id,
+                            format,
+                            email)
+
+  def get_report_status(self, report_id):
+    """`<https://docs.exchange.coinbase.com/#get-report-status>`_"""
+    return self._get('reports', report_id)
 
 class PrivatePaginationClient(PaginationClient):
   def __init__(self, auth, api_uri=None, before=True, limit=None, cursor=None):
@@ -111,14 +285,26 @@ class PrivatePaginationClient(PaginationClient):
 
     """
     uri = self._create_api_uri(*relative_path_parts)
-    response = get(uri, auth=self.auth, params=self._get_params(**kwargs))
+    if method == 'get':
+      response = get(uri, auth=self.auth, params=kwargs.get('params', None))
+    elif method == 'post':
+      response = post(uri, auth=self.auth, json=kwargs.get('data', None))
+    else:
+      response = delete(uri, auth=self.auth, json=kwargs.get('data', None))
     self.is_initial = False
     self.before_cursor = response.headers.get('cb-before', None)
     self.after_cursor = response.headers.get('cb-after', None)
     return self._handle_response(response).json()
 
-class GetAccountHistoryPagination(PrivatePaginationClient):
-  """`<https://docs.exchange.coinbase.com/#get-account-history>`_"""
+class ListAccountsPagination(PrivatePaginationClient):
+  """`<https://docs.exchange.coinbase.com/#selecting-a-timestamp>`_"""
+  def endpoint(self):
+    if self._check_next():
+      return self._get('accounts')
+    else:
+      return None
+
+class AccountPagination(PrivatePaginationClient):
   def __init__(self,
                account_id,
                auth,
@@ -126,17 +312,77 @@ class GetAccountHistoryPagination(PrivatePaginationClient):
                before=True,
                limit=None,
                cursor=None):
-    super(GetAccountHistoryPagination, self).__init__(auth,
-                                                      api_uri,
-                                                      before,
-                                                      limit,
-                                                      cursor)
+    super(AccountPagination, self).__init__(auth,
+                                            api_uri,
+                                            before,
+                                            limit,
+                                            cursor)
     self.account_id = account_id
 
+class GetAccountHistoryPagination(AccountPagination):
+  """`<https://docs.exchange.coinbase.com/#get-account-history>`_"""
   def endpoint(self):
     if self._check_next():
       return self._get('accounts', self.account_id, 'ledger')
     else:
       return None
 
-# TODO - ALL LIST RETURNS SUPPORT PAGINATION SO CREATE A PAGINATION CLIENT FOR EACH
+class GetHoldsPagination(AccountPagination):
+  """`<https://docs.exchange.coinbase.com/#get-holds>`_"""
+  def endpoint(self):
+    if self._check_next():
+      return self._get('accounts', self.account_id, 'holds')
+    else:
+      return None
+
+class ListFillsPagination(PrivatePaginationClient):
+  """`<https://docs.exchange.coinbase.com/#list-fills>`_"""
+  def endpoint(self):
+    if self._check_next():
+      return self._get('fills')
+    else:
+      return None
+
+class CancelAllPagination(PrivatePaginationClient):
+  """`<https://docs.exchange.coinbase.com/#cancel-all>`_"""
+  def __init__(self,
+               product_id,
+               auth,
+               api_uri=None,
+               before=True,
+               limit=None,
+               cursor=None):
+    super(AccountPagination, self).__init__(auth,
+                                            api_uri,
+                                            before,
+                                            limit,
+                                            cursor)
+    self.product_id = product_id
+
+  def endpoint(self):
+    if self._check_next():
+      return self._delete('orders', data={'product_id':self.product_id})
+    else:
+      return None
+
+class ListOrdersPagination(PrivatePaginationClient):
+  """`<https://docs.exchange.coinbase.com/#list-orders>`_"""
+  def __init__(self,
+               auth,
+               status=None,
+               api_uri=None,
+               before=True,
+               limit=None,
+               cursor=None):
+    super(ListOrdersPagination, self).__init__(auth,
+                                               api_uri,
+                                               before,
+                                               limit,
+                                               cursor)
+    self.status = status
+
+  def endpoint(self):
+    if self._check_next():
+      return self._get('orders', params={'status':self.status})
+    else:
+      return None
